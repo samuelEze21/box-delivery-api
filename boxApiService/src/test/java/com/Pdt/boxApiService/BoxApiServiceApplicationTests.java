c8fae368f5d42839b199d0885283ba9dea31b516
Flosamuel <nwabuezesamuel21@gmail.com>
2025-08-17 04:25:08 +0100

    create package structure and add model classes

---
+++ boxApiService/src/test/java/com/Pdt/boxApiService/BoxApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pdt.boxApiService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
